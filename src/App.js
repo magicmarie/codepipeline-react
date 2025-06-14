8de8b042d6bc2a41e44617464c4e1534794ce05b
magicmarie <natukunda162@gmail.com>
2025-06-13 20:36:46 -0500

    update the js file

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <h1>Adding something here to see if the automatic builds work</h1>
+      <h1>adding more</h1>
     </div>
   );
 }
